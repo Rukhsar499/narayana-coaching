a42ce94c52a270f1bfb3040a4393fc59aa39b3b7
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-03-17 11:30:57 +0530

    narayana-coaching

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+// slider js
+ var $carousel = $('[data-owl-carousel]');
+ if ($carousel.length) {
+     $carousel.each(function (index, el) {
+         $(this).owlCarousel($(this).data('owl-carousel'));
+     });
+ }
+
+
+
+
+
+    
+  
+    
+
+
+
+// data aos
+
+
+  AOS.init();
+  AOS.refresh(); // initialize AOS animations
+  $(document).ready(function(){
+      $("body,html").animate({
+          scrollTop: 10
+      }, 1000);
+      $("body,html").animate({
+          scrollTop: 0
+      }, 1);
+  });
+
+// close-btn
+function closeSection() {
+  document.getElementById("closeable-section").style.display = "none";
+}
